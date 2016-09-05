275aeaea62571f58ddd71c6f7af9fc8973e919d3
Arda Özkal <ardaozkal@users.noreply.github.com>
2016-09-05 03:40:09 +0300

    Update PythonBackend.py

--- PythonBackend.py
+++ PythonBackend.py
@@ -1,14 +1,14 @@
 import sqlite3
 import urllib2
 
-conn = sqlite3.connect('VACUpdate.sqlite')
+conn = sqlite3.connect('/home/ardaoftp/samba/VACUpdate.sqlite')
 c = conn.cursor()
 
 def vacthingy( input ):
    url = 'http://steamcommunity.com/profiles/'+str(input)+'?xml=1'
    webFile = urllib2.urlopen(url).read()
    if "<vacBanned>0</vacBanned>" not in webFile: 
-           c.execute("UPDATE trackedusers SET banned = 1 WHERE steamid=?",str(input))
+           c.execute("UPDATE trackedusers SET banned = 1 WHERE steamid=?",(str(input),))
            print str(input) + " is banned";
    return
 
